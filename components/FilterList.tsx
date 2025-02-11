9d4e2c54e23c4ab0286cfd350c3360707d75bc3e
Shuto Tamaoka <shuto.tamaoka@gmail.com>
2025-02-11 20:33:23 +0900

    fix: make gap size consistent for filter checkbox

--- components/FilterList.tsx
+++ components/FilterList.tsx
@@ -86,7 +86,7 @@ const FilterList = () => {
           ステラー
         </label>
       </div>
-      <div className="flex justify-center gap-1">
+      <div className="flex justify-center gap-2 mt-1">
         <label>
           <input
             type="checkbox"
