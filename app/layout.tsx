0e52866083c193665a89b7b0c0e4013341d25459
Shuto Tamaoka <shuto.tamaoka@gmail.com>
2025-02-13 14:45:49 +0900

    feat: use BIZ UDGothic as default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { BIZ_UDGothic } from "next/font/google";
+
+const bizUdGothic = BIZ_UDGothic({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "アリス・ギア・アイギス スカウト率チェッカー",
@@ -13,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="antialiased text-black">{children}</body>
+      <body className={`${bizUdGothic.className} antialiased text-black`}>
+        {children}
+      </body>
     </html>
   );
 }
