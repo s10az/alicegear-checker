3c1b412ba9fa8f419fabb1f33e33ac6396887f26
Shuto Tamaoka <shuto.tamaoka@gmail.com>
2025-02-13 14:13:28 +0900

    update: remove Geist font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "アリス・ギア・アイギス スカウト率チェッカー",
@@ -24,11 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-black`}
-      >
-        {children}
-      </body>
+      <body className="antialiased text-black">{children}</body>
     </html>
   );
 }
