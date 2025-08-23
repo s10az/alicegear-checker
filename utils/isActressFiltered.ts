6bb5a8e095830446ea46e1ff26d5a9fae32131b2
Shuto Tamaoka <shuto.tamaoka@gmail.com>
2025-08-23 19:57:10 +0900

    Refactoring

--- utils/isActressFiltered.ts
+++ utils/isActressFiltered.ts
@@ -9,29 +9,21 @@ export function isActressFiltered(
   let isKindMatching: boolean = false;
   let isAttributeMatching: boolean = false;
 
-  if (filter.normal && actress.kind == "normal") {
-    isKindMatching = true;
-  }
-  if (filter.another && actress.kind == "another") {
-    isKindMatching = true;
-  }
-  if (filter.factor && actress.kind == "factor") {
-    isKindMatching = true;
-  }
-  if (filter.stellar && actress.kind == "stellar") {
+  if (
+    (filter.normal && actress.kind == "normal") ||
+    (filter.another && actress.kind == "another") ||
+    (filter.factor && actress.kind == "factor") ||
+    (filter.stellar && actress.kind == "stellar")
+  ) {
     isKindMatching = true;
   }
 
-  if (filter.electric && actress.attribute == "electric") {
-    isAttributeMatching = true;
-  }
-  if (filter.gravity && actress.attribute == "gravity") {
-    isAttributeMatching = true;
-  }
-  if (filter.heat && actress.attribute == "heat") {
-    isAttributeMatching = true;
-  }
-  if (filter.freeze && actress.attribute == "freeze") {
+  if (
+    (filter.electric && actress.attribute == "electric") ||
+    (filter.gravity && actress.attribute == "gravity") ||
+    (filter.heat && actress.attribute == "heat") ||
+    (filter.freeze && actress.attribute == "freeze")
+  ) {
     isAttributeMatching = true;
   }
 
